44703c21185542bbd7ca132cb9936caf86682053
Bruno <bcoronado.morales@gmail.com>
2019-08-11 02:13:13 -0600

    Irony agregado e inicio de la estructura de las gramaticas, iniciado Lup

---
+++ CQL-Teacher-Servidor/Analisis/Lup/Gramatica_Lup.cs
@@ -0,0 +1,14 @@
+﻿using Irony.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQL_Teacher_Servidor.Analisis.Lup //namespace no acepta - -> remplazar por _
+{
+    public class Gramatica_Lup : Grammar
+    {
+
+        //DEFINIR LA GRAMATICA PARA LUP
+    }
+}
